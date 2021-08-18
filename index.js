a8bb1ed9a77834beb90efbcb64d78f796167c30b
staedtlernorica-R4 <longuong@live.com>
2021-08-18 14:21:15 -0400

    added mroe jQuery

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-function valueOf(id) {
-    if (document.getElementById(id).type == 'text') {
-        return Number(document.getElementById(id).value.trim());
-    } else if (document.getElementById(id).type == 'radio') {
-        return document.getElementById(id).checked;
-    };
+function clearInputs(className) {
+    let selector = `input.${className}`;
+    $(selector).val(null);
+    $(selector).prop('checked', false);
+};
+
+
+function clearAllInputs() {
+    $('input[type="text"]').val(null);
+    $('input[type="radio"]').prop('checked', false);
+}
+
+
+// very robust against whitespaces; 1 1 32 hrs == 1132 hrs;
+function value(id) {
+    if ($(`#${id}`).attr('type') == 'radio'){
+        return $(`#${id}`).prop('checked'); 
+    }
+    return Number($(`#${id}`).val().replace(/\s/g,''));
 }
 
 
@@ -24,15 +37,15 @@ function metricImperialFactor(inputField){
 function baseUnit(unit = '') {
 
     if (unit == 'sec') {
-        let time = valueOf('hour') * 3600 + valueOf('min') * 60 + valueOf('sec');
+        let time = value('hour') * 3600 + value('min') * 60 + value('sec');
         return time;
 
     } else if (unit == 'meter') {
         let outputUnit = metricImperialFactor('distance')
-        return Number(valueOf('distance') * outputUnit);
+        return Number(value('distance') * outputUnit);
 
     } else if (unit == 'secPerMeter') {
-        let paceSecPerMeter = valueOf('pace-min') * 60 + valueOf('pace-sec');
+        let paceSecPerMeter = value('pace-min') * 60 + value('pace-sec');
         let outputUnit = metricImperialFactor('pace');
         return (paceSecPerMeter/outputUnit);
     }
@@ -84,7 +97,7 @@ function calcPace() {
 
 
 function validNumber(id, checkInteger = false) {
-    let number = valueOf(id);
+    let number = value(id);
     let isPositiveNumber = !isNaN(Number(number)) && number > 0;
     let isInteger = Number.isInteger(number);
 
@@ -98,62 +111,44 @@ function validNumber(id, checkInteger = false) {
 
 function calculate() {
     // at least one time filled and above zero                 cond 2    
-    let timeFilled = validNumber('hour', true) || validNumber('min', true) || validNumber('sec', true);
+    let timeFilled = condition2 = validNumber('hour', true) || validNumber('min', true) || validNumber('sec', true);
 
     // distance filled and above zero, and checked              cond 1
     let distanceFilled = validNumber('distance');
     let distanceChecked = $('.distance')[1].checked || $('.distance')[2].checked;
+    let condition1 = distanceFilled && distanceChecked;
 
     // at least one pace filled and above zero, and checked     cond 3
     let paceFilled = validNumber('pace-min', true) || validNumber('pace-sec', true);
     let paceChecked = $('.pace')[2].checked || $('.pace')[3].checked;
+    let condition3 = paceFilled && paceChecked;
 
 
-    // appropriate empty input fields
-    let timeEmpty = valueOf('hour') + valueOf('min') + valueOf('sec') === 0;
-    let distanceEmptyButChecked = (valueOf('distance') === 0) && distanceChecked;
-    let paceEmptyButChecked = (valueOf('pace-min') + valueOf('pace-sec') === 0) && paceChecked;
+    // appropriate empty input fields for specific conditions
+    let timeEmpty = value('hour') + value('min') + value('sec') === 0;
+    let distanceEmptyButChecked = (value('distance') === 0) && distanceChecked;
+    let paceEmptyButChecked = (value('pace-min') + value('pace-sec') === 0) && paceChecked;
 
 
-    if (timeEmpty && distanceFilled && distanceChecked && paceFilled && paceChecked) {
+    if (timeEmpty && condition1 && condition3) {
         // time empty                                           calc time
         // distance filled, above zero and checked              cond 1
         // at least one pace filled, above zero and checked     cond 3
         calcTime();
     }
-    else if (paceEmptyButChecked && timeFilled && distanceFilled && distanceChecked) {
+    else if (paceEmptyButChecked && condition1 && condition2) {
         // pace empty but checked                               calc pace
         // at least one time filled, above zero                 cond 2
         // distance filled, above zero and checked              cond 1
         calcPace();
     }
-    else if (distanceEmptyButChecked && timeFilled && paceFilled && paceChecked) {
+    else if (distanceEmptyButChecked && condition2 && condition3) {
         // distance empty but checcked                          calc dist
         // at least one time filled, above zero                 cond 2
         // at least one pace filled, above zero and checked     cond 3
         calcDistance();
     }
     else {
-        alert('Invalid inputs! Try again. \n\n Make sure there are no letters, or spaces/tabs between numbers');
+        alert('Invalid inputs! Try again.');
     }
-}
-
-
-function clearInputs(field) {
-
-    for (i = 0; i < document.getElementsByClassName(field).length; i++) {
-
-        if (document.getElementsByClassName(field)[i].type == 'text') {
-            document.getElementsByClassName(field)[i].value = null;
-        } else if (document.getElementsByClassName(field)[i].type == 'radio') {
-            document.getElementsByClassName(field)[i].checked = null;
-        }
-    }
-};
-
-
-function clearValue() {
-    clearInputs('time');
-    clearInputs('distance');
-    clearInputs('pace');
 }
