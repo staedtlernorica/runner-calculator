dadb6c95d71db2e38dccc8744749a84d176fb7c2
staedtlernorica-R4 <longuong@live.com>
2021-08-27 00:33:00 -0400

    major rewrite

--- index.js
+++ index.js
@@ -3,47 +3,82 @@ function clearInputs(className) {
 };
 
 function clearAllInputs() {
-    $('input[type="text"]').val(null);
+    $('input[type="number"]').val(null);
 }
 
+function changeUnit(unitButton) {
 
-function changeUnit(button){
+    if (unitButton.id === 'distance-unit') {
+        unitButton.innerHTML === 'km' ? (unitButton.innerHTML = 'mi', distanceUnit = 1609.34) : (unitButton.innerHTML = 'km', distanceUnit = 1000);
 
-    if (button.id === 'distance-unit'){
-        button.innerHTML === 'km' ? (button.innerHTML = 'mi', distanceUnit = 1609.34) : (button.innerHTML = 'km', distanceUnit = 1000);
-
-    } else if (button.id === 'pace-unit'){
-        button.innerHTML === 'min/km' ? (button.innerHTML = 'min/mi', paceUnit = 1609.34) : (button.innerHTML = 'min/km', paceUnit = 1000);
+    } else if (unitButton.id === 'pace-unit') {
+        unitButton.innerHTML === 'min/km' ? (unitButton.innerHTML = 'min/mi', paceUnit = 1609.34) : (unitButton.innerHTML = 'min/km', paceUnit = 1000);
     }
-}
-
-
-// very robust against spaces/tabs; 1 1 32 hrs == 1132 hrs;
-function value(object) {
-    // return Number($(`#${id}`).val().replace(/\s/g,''));
-    return Number(object.val().replace(/\s/g, ''));
 }
 
 
 let distanceUnit = 1000;
 let paceUnit = 1000;
 
+function calculateTime(smallest) {
+    let timeInSec = Math.round(smallest.distance * smallest.pace);
+
+    $('#hour').val(Math.floor(timeInSec / 3600));
+    $('#min').val(Math.floor(timeInSec % 3600 / 60));
+    $('#sec').val(Math.floor(timeInSec % 3600 % 60));
+}
+
+function calculateDistance(smallest) {
+    let distInMeter = smallest.time / smallest.pace;
+    let outputDistance = Math.round(distInMeter / distanceUnit * 100) / 100;
+    $('#distance').val(outputDistance);
+}
+
+function calculatePace(smallest) {
+    let paceInSecPerMeter = smallest.time / smallest.distance;
+    let decimalPace = paceInSecPerMeter * paceUnit / 60;
+
+    // floor instead of round b/c Math.round(6.7) = 7 mins rather than 6min 42sec
+    let paceMin = Math.floor(decimalPace);
+    // Math.round((x)*60) vs Math.round(x)*60 b/c dont want to round to early;
+    // the latter gives 1 hour/60km = 2min/mi when really 1:37min/mile
+    let paceSec = Math.round((decimalPace - paceMin) * 60);
+
+    // avoid outputs like 7min 60sec; get 8 min 0 sec
+    paceSec === 60 ? (paceMin = paceMin + 1, paceSec = 0) : null;
+
+    $('#pace-min').val(paceMin);
+    $('#pace-sec').val(paceSec);
+}
+
+
+function correctInputs(userInputs) {
+    let correctScore = 0;
+    for (let key in userInputs){
+        //add 1 to correctScore if input is 0 or higher
+        userInputs[key] >= 0 ? correctScore++ : null;   
+
+        // add 1 to correctScore if all inputs except distance is integer (dev choice)
+        if (key !== 'distance' && Number.isInteger(userInputs[key])){
+            correctScore++;
+        }
+    }
+    if(correctScore === 11){
+        return true;
+    }
+    return false;
+}
+
 
 function calculate() {
+    // need + sign in front to make it a number
     let userInputs = {
-        hour: value($("#hour")),
-        minute: value($("#min")),
-        second: value($("#sec")),
-        distance: value($("#distance")),  
-        paceMinute: value($("#pace-min")),
-        paceSecond: value($("#pace-sec")),
-        // maybe eliminate paceAboveZero and timeAboveZero, redundant
-
-        timeAboveZero: value($("#hour")) + value($("#min")) + value($("#sec")),
-        timeInSeconds: value($("#hour"))*3600 + value($("#min"))*60 + value($("#sec")),
-
-        paceAboveZero: value($("#pace-min")) + value($("#pace-sec")),
-        paceInSecondsPerMeter: value($("#pace-min")) * 60 + value($("#pace-sec"))
+        hour: +$("#hour").val(),
+        minute: +$("#min").val(),
+        second: +$("#sec").val(),
+        distance: +$("#distance").val(),
+        paceMinute: +$("#pace-min").val(),
+        paceSecond: +$("#pace-sec").val(),
     };
 
     let smallest = {
@@ -52,87 +87,28 @@ function calculate() {
         pace: (userInputs.paceMinute * 60 + userInputs.paceSecond) / paceUnit
     }
 
-    let conditions = {
+    let timeFilled = smallest.time > 0;
+    let distanceFilled = smallest.distance > 0;
+    let paceFilled = smallest.pace > 0;
 
-    }
+    // check for correct inputs, then correct conditions
+    if (correctInputs(userInputs)) {
 
-    // console.log(userInputs);
-
-    let timeFilled = userInputs.timeAboveZero > 0;
-
-    let distanceChecked = userInputs.distanceUnit != 0;
-    let distanceFilled = userInputs.distance > 0;
-    let distanceCheckedFilled = distanceChecked && distanceFilled;
-    let distanceCheckedEmpty = distanceChecked && !distanceFilled;
-
-    let paceChecked = userInputs.paceUnit != 0;
-    let paceFilled = userInputs.paceAboveZero > 0;
-    let paceCheckedFilled = paceChecked && paceFilled;
-    let paceCheckedEmpty = paceChecked && !paceFilled;
-
-    // have 3 shared conditions and 3 specific conditions
-    if (!timeFilled && distanceCheckedFilled && paceCheckedFilled) {
-
-        let timeInSec = Math.round(smallest.distance * smallest.pace);
-
-        $('#hour').val(Math.floor(timeInSec / 3600));
-        $('#min').val(Math.floor(timeInSec % 3600 / 60));
-        $('#sec').val(Math.floor(timeInSec % 3600 % 60));
-    }
-    else if (paceCheckedEmpty && timeFilled && distanceCheckedFilled) {
-
-        let paceInSecPerMeter = smallest.time / smallest.distance;
-        let decimalPace = paceInSecPerMeter * paceUnit / 60;
-
-        // floor instead of round b/c Math.round(6.7) = 7 mins rather than 6min 42sec
-        let paceMin = Math.floor(decimalPace);
-        // Math.round((x)*60) vs Math.round(x)*60 b/c dont want to round to early;
-        // the latter gives 1 hour/60km = 2min/mi when really 1:37min/mile
-        let paceSec = Math.round((decimalPace - paceMin) * 60);
-
-        // avoid outputs like 7min 60sec; get 8 min 0 sec
-        if (paceSec == 60) {
-            paceMin = paceMin + 1;
-            paceSec = 0;
+        // have 3 shared conditions and 3 specific conditions
+        // time unfilled,   distance filled,    pace filled
+        // time filled,     distance filled,    pace unfilled
+        // time filled,     distance unfilled,  pace filled
+        if (!timeFilled && distanceFilled && paceFilled) {
+            calculateTime(smallest);
         }
-
-        $('#pace-min').val(paceMin);
-        $('#pace-sec').val(paceSec);
-    }
-    else if (distanceCheckedEmpty && timeFilled && paceCheckedFilled) {
-
-        let distInMeter = smallest.time / smallest.pace;
-        let outputDistance = Math.round(distInMeter / distanceUnit * 100) / 100;
-        $('#distance').val(outputDistance);
-    }
-    else {
-        alert('Invalid inputs! Try again.');
+        else if (timeFilled && distanceFilled && !paceFilled) {
+            calculatePace(smallest);
+        }
+        else if (timeFilled && !distanceFilled && paceFilled) {
+            calculateDistance(smallest);
+        }
+        else {
+            alert('Invalid inputs! Try again.');
+        }
     }
 }
-
-        // time empty                                           calc time
-        // distance filled, above zero and checked              cond 1
-        // at least one pace filled, above zero and checked     cond 3
-
-        // pace empty but checked                               calc pace
-        // at least one time filled, above zero                 cond 2
-        // distance filled, above zero and checked              cond 1
-
-        // distance empty but checcked                          calc dist
-        // at least one time filled, above zero                 cond 2
-        // at least one pace filled, above zero and checked     cond 3
-
-
-
-// function validNumber(id, checkInteger = false) {
-//     let number = value(id);
-//     let isPositiveNumber = !isNaN(Number(number)) && number > 0;
-//     let isInteger = Number.isInteger(number);
-
-//     if (checkInteger == true) {
-//         return (isPositiveNumber && isInteger);
-//     }
-
-//     return (isPositiveNumber)
-// }
-
